68d2a39ff4ed8386ea8116cdbc0d080776b9da95
Juan Carlos Valdés <idevkingos@gmail.com>
2018-07-11 22:42:54 -0300

    ng sin redux

---
+++ 02-ng-sin-redux-app/src/app/contador/nieto/nieto.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nieto',
+  templateUrl: './nieto.component.html',
+  styles: []
+})
+export class NietoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() resetContador = new EventEmitter<number>();
+  constructor() {}
+
+  ngOnInit() {}
+  reset() {
+    this.contador = 0;
+    this.resetContador.emit(0);
+  }
+}
